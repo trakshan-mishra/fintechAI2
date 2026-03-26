e4f118cd2a15dddbb16620ebe44fd0bef57ff3c6
Trakshan Mishra <43599000+trakshan-mishra@users.noreply.github.com>
2026-03-26 12:57:49 +0530

    Update firebase.js

--- frontend/src/utils/firebase.js
+++ frontend/src/utils/firebase.js
@@ -5,13 +5,13 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 // Firebase config
 const firebaseConfig = {
-  apiKey: "AIzaSyD8LVvcCFaAl-SjIUqtLB_xF52NCi7oHP4",
-  authDomain: "capsaf-3ae54.firebaseapp.com",
-  projectId: "capsaf-3ae54",
-  storageBucket: "capsaf-3ae54.firebasestorage.app",
-  messagingSenderId: "1061999838819",
-  appId: "1:1061999838819:web:97e4468db09c3dbd351a56",
-  measurementId: "G-265M3M338Z"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize app
